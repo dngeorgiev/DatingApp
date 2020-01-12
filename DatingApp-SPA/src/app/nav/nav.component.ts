8f0ca6ddb836e37ac7ce5d9315c87adc7e8373c8
Dani Nedelchev <dngeorgiev@abv.bg>
2020-01-12 17:17:02 +0200

    Added alertifyjs for notifications for the app.

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,37 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../_services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../_services/auth.service";
+import { AlertifyService } from "../_services/alertify.service";
 
 @Component({
-	selector: 'app-nav',
-	templateUrl: './nav.component.html',
-	styleUrls: [ './nav.component.css' ]
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.css"]
 })
 export class NavComponent implements OnInit {
-	model: any = {};
+  model: any = {};
 
-	constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
-	ngOnInit() {}
+  ngOnInit() {}
 
-	login() {
-		this.authService.login(this.model).subscribe(
-			(next) => {
-				console.log('logged in successfully!');
-			},
-			(error) => {
-				console.log(error);
-			}
-		);
-	}
+  login() {
+    this.authService.login(this.model).subscribe(
+      next => {
+        this.alertify.success("You have successfully logged in.");
+      },
+      error => {
+        this.alertify.error(error);
+      }
+    );
+  }
 
-	loggedIn() {
-		const token = localStorage.getItem('token');
+  loggedIn() {
+    const token = localStorage.getItem("token");
 
-		return !!token;
-	}
+    return !!token;
+  }
 
-	logout() {
-		localStorage.removeItem('token');
-		console.log('logged out!');
-	}
+  logout() {
+    localStorage.removeItem("token");
+    this.alertify.success("You have successfully logged out.");
+  }
 }
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,34 +1,36 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { AuthService } from '../_services/auth.service';
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { AuthService } from "../_services/auth.service";
+import { AlertifyService } from "../_services/alertify.service";
 
 @Component({
-	selector: 'app-register',
-	templateUrl: './register.component.html',
-	styleUrls: [ './register.component.css' ]
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent implements OnInit {
-	@Output() cancelRegister = new EventEmitter();
+  @Output() cancelRegister = new EventEmitter();
 
-	model: any = {};
+  model: any = {};
 
-	constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
-	ngOnInit() {}
+  ngOnInit() {}
 
-	register() {
-		this.authService.register(this.model).subscribe(
-			() => {
-				console.log('registration successful');
-			},
-			(error) => {
-				console.log(error);
-			}
-		);
-	}
+  register() {
+    this.authService.register(this.model).subscribe(
+      () => {
+        this.alertify.success("You have successfully registered.");
+      },
+      error => {
+        this.alertify.error(error);
+      }
+    );
+  }
 
-	cancel() {
-		this.cancelRegister.emit(false);
-
-		console.log('canceled');
-	}
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
 }
